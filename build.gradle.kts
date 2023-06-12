10b8f20125630b2f723dac21c7b98cf2b98c5559
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-06-12 18:17:53 +0900

    add scripts dir

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,15 @@ plugins {
     alias(libs.plugins.nav.safeargs.kotlin) apply false
     alias(libs.plugins.kapt) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+buildscript {
+    dependencies {
+        this.classpath("io.github.gradle-nexus.publish-plugin:publish-plugin:1.1.0")
+    }
+}
+
+apply {
+    from("publish-root.gradle.kts")
 }
