5af8139de11f4597cea723e021282e3d801470d6
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-06-23 01:41:19 +0900

    completed floating card view to first alpha version

--- feature/holographic/src/main/java/io/github/pknujsp/testbed/feature/holographic/ImageFragment.kt
+++ feature/holographic/src/main/java/io/github/pknujsp/testbed/feature/holographic/ImageFragment.kt
@@ -10,64 +10,71 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import io.github.pknujsp.testbed.feature.holographic.databinding.FragmentImageBinding
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 
 class ImageFragment : Fragment() {
 
-    private var _binding: FragmentImageBinding? = null
-    private val binding get() = _binding!!
+  private var _binding: FragmentImageBinding? = null
+  private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentImageBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    _binding = FragmentImageBinding.inflate(inflater, container, false)
+    return binding.root
+  }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        binding.apply {
+    binding.apply {
 
-            /*
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                viewLifecycleOwner.lifecycleScope.launch {
-                    viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                        binding.root.doOnLayout {
-                            ColorAnalyzer().analyzeColor(statusTextview, imageview,
-                                resources.getColor(com.pknujsp.core.ui.R.color.card_shadow_color, null))
-                        }
-                    }
-                }
-            }
+      /*
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+          viewLifecycleOwner.lifecycleScope.launch {
+              viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                  binding.root.doOnLayout {
+                      ColorAnalyzer().analyzeColor(statusTextview, imageview,
+                          resources.getColor(com.pknujsp.core.ui.R.color.card_shadow_color, null))
+                  }
+              }
+          }
+      }
 
-             */
+       */
 
-            viewLifecycleOwner.lifecycleScope.launch {
-                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    val outlineProvider = OutlineProvider(0, 0)
-                    imageview.outlineProvider = outlineProvider
-                    imageview.clipToOutline = true
-                    imageview.invalidate()
+      viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+          val outlineProvider = OutlineProvider(0, 0)
+          backgroundCardview.outlineProvider = outlineProvider
+          backgroundCardview.clipToOutline = true
+          backgroundCardview.invalidate()
 
-                    ShadowManager(requireContext().applicationContext,
-                        viewLifecycleOwner.lifecycle,
-                        SupervisorJob()).gravityChannel.collectLatest { gravity ->
+          val imageViewX = imageview.x
+          val imageViewY = imageview.y
 
-                        val status = "$gravity  ->  Image : ${imageview.width} x ${imageview.height}  -> " +
-                                " Outline : ${outlineProvider.rect.width()} x " + "${outlineProvider.rect.height()}"
-                        statusTextview.text = status
-                        imageview.invalidateOutline()
-                    }
-                }
-            }
+          ShadowManager(
+            requireContext().applicationContext,
+            viewLifecycleOwner.lifecycle,
+            SupervisorJob(),
+          ).sensorValueChannel.collect { changedValue ->
+            statusTextview.text = changedValue.toString()
+            outlineProvider.shiftY = changedValue.shadowShiftY
+            outlineProvider.shiftX = changedValue.shadowShiftX
 
+            imageview.translationX = imageViewX + changedValue.imageShiftX
+            imageview.translationY = imageViewY + changedValue.imageShiftY
+
+            backgroundCardview.invalidateOutline()
+          }
         }
-    }
+      }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
 
 }
--- feature/holographic/src/main/java/io/github/pknujsp/testbed/feature/holographic/OutlineProvider.kt
+++ feature/holographic/src/main/java/io/github/pknujsp/testbed/feature/holographic/OutlineProvider.kt
@@ -6,26 +6,18 @@ import android.view.View
 import android.view.ViewOutlineProvider
 
 internal class OutlineProvider(
-    var shiftX: Int = 0, var shiftY: Int = 0
+  var shiftX: Int = 0, var shiftY: Int = 0,
 ) : ViewOutlineProvider() {
 
-    val rect: Rect = Rect()
+  private val rect: Rect = Rect()
 
-    override fun getOutline(view: View, outline: Outline?) {
-        rect.set(-20, -20, view.width + 20, view.height - 5)
-        if (shiftX > 0) {
-            rect.right += shiftX
-        } else {
-            rect.left += shiftX
-        }
+  override fun getOutline(view: View, outline: Outline?) {
+    rect.set(-10, 10, view.width + 10, view.height)
+    rect.right += shiftX
+    rect.left += shiftX
 
-        if (shiftY > 0) {
-            rect.bottom += shiftY
-        } else {
-            rect.top += shiftY
-        }
-
-        outline?.setRect(rect)
-        outline?.alpha = 1f
-    }
+    rect.bottom += shiftY
+    rect.top += shiftY
+    outline?.setRect(rect)
+  }
 }
