56937ae1914f8f953748d8677dfb32ab89d67206
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-06-22 21:50:25 +0900

    fix errors

--- simpledialog/build.gradle.kts
+++ simpledialog/build.gradle.kts
@@ -23,8 +23,12 @@ android {
 
 rootProject.extra.apply {
   set("PUBLISH_ARTIFACT_ID", "simpledialog")
-  set("PUBLISH_DESCRIPTION", "Simple dialog library")
   set("PUBLISH_VERSION", "1.0.0")
+  set("PUBLISH_DESCRIPTION", "Android Simple Dialog Library")
+  set("PUBLISH_URL", "https://github.com/pknujsp/android-simpledialog")
+  set("PUBLISH_SCM_CONNECTION", "scm:git:github.com/pknujsp/android-simpledialog")
+  set("PUBLISH_SCM_DEVELOPER_CONNECTION", "scm:git:ssh://github.com/pknujsp/android-simpledialog.git")
+  set("PUBLISH_SCM_URL", "https://github.com/pknujsp/android-simpledialog.git")
 }
 
 tasks.withType(GenerateModuleMetadata::class).configureEach {
