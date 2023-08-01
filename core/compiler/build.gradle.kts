d27189fa380085be796fcb6e170077194ad63ae1
JSPark <48265129+pknujsp@users.noreply.github.com>
2023-08-01 17:47:34 +0900

    KSealedBinding 배포 성공

--- core/compiler/build.gradle.kts
+++ core/compiler/build.gradle.kts
@@ -1,6 +1,21 @@
 plugins {
   alias(libs.plugins.kotlin.jvm)
   alias(libs.plugins.ksp)
+  alias(libs.plugins.dokka)
+}
+
+rootProject.extra.apply {
+  set("PUBLISH_ARTIFACT_ID", "ksealedbinding-compiler")
+  set("PUBLISH_VERSION", "1.0.0")
+  set("PUBLISH_DESCRIPTION", "compiler of KSealedBinding Library")
+  set("PUBLISH_URL", "https://github.com/pknujsp/KSealedBinding")
+  set("PUBLISH_SCM_CONNECTION", "scm:git:github.com/pknujsp/KSealedBinding.git")
+  set("PUBLISH_SCM_DEVELOPER_CONNECTION", "scm:git:ssh://github.com/pknujsp/KSealedBinding.git")
+  set("PUBLISH_SCM_URL", "https://github.com/pknujsp/KSealedBinding.git")
+}
+
+apply {
+  from("${rootProject.projectDir}/scripts/publish-jvm-module.gradle")
 }
 
 java {
